051145826be0e7e892016ed7bb104de55a867256
codebymks <228756637+codebymks@users.noreply.github.com>
2025-11-12 20:41:45 +0100

    Add files via upload

---
+++ projectplanner/src/main/java/dk/ek/projectplanner/ProjectplannerApplication.java
@@ -0,0 +1,13 @@
+package dk.ek.projectplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectplannerApplication.class, args);
+	}
+
+}
