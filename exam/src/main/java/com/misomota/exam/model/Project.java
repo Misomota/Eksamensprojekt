30f5ebfbd70bfc2fe88215add9d34786bdb238fe
miso0007 <miso0007@stud.kea.dk>
2025-11-18 15:01:31 +0100

    Tilføjet Account og Project klasser til model mappe

--- exam/src/main/java/com/misomota/exam/model/Project.java
+++ exam/src/main/java/com/misomota/exam/model/Project.java
@@ -1,4 +1,27 @@
 package com.misomota.exam.model;
 
 public class Project {
+    private String projectName;
+    private int projectID;
+
+    public Project(String projectName, int projectID) {
+        this.projectName = projectName;
+        this.projectID = projectID;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public int getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(int projectID) {
+        this.projectID = projectID;
+    }
 }
