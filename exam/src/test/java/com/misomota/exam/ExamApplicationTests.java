36c53cbb14ef505926e755a18e05b209027a8a9a
miso0007 <miso0007@stud.kea.dk>
2025-12-02 21:19:50 +0100

    ændret i test

--- exam/src/test/java/com/misomota/exam/ExamApplicationTests.java
+++ exam/src/test/java/com/misomota/exam/ExamApplicationTests.java
@@ -2,8 +2,10 @@ package com.misomota.exam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExamApplicationTests {
 
 	@Test
